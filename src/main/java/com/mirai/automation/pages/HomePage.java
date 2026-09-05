5faafbc083445244d2b06fc3bd8dd984152d7587
tkhamishs <198059916+tkhamishs@users.noreply.github.com>
2026-09-05 10:37:25 +0300

    Add desktop login entry flow

--- src/main/java/com/mirai/automation/pages/HomePage.java
+++ src/main/java/com/mirai/automation/pages/HomePage.java
@@ -3,21 +3,45 @@ package com.mirai.automation.pages;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.WaitForSelectorState;
 
 public class HomePage {
 
     private final Page page;
     private final Locator acceptCookiesButton;
+    private final Locator menuButton;
+    private final Locator loginButton;
 
     public HomePage(Page page) {
         this.page = page;
+
         this.acceptCookiesButton = page.getByRole(
                 AriaRole.BUTTON,
                 new Page.GetByRoleOptions().setName("Accept All")
+        );
+
+        this.menuButton = page.locator(
+                "nav ul button:has(img[alt='avatar'])"
+        );
+
+        this.loginButton = page.getByRole(
+                AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Login")
         );
     }
 
     public void acceptCookies() {
         acceptCookiesButton.click();
+    }
+
+    public void openLogin() {
+        menuButton.hover();
+
+        loginButton.waitFor(
+                new Locator.WaitForOptions()
+                        .setState(WaitForSelectorState.VISIBLE)
+        );
+
+        loginButton.click();
     }
 }
--- src/test/java/com/mirai/automation/tests/HomePageSmokeTest.java
+++ src/test/java/com/mirai/automation/tests/HomePageSmokeTest.java
@@ -4,24 +4,27 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.mirai.automation.config.Config;
 import com.mirai.automation.pages.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import com.mirai.automation.config.Config;
 
 public class HomePageSmokeTest {
 
     @Test
     public void shouldOpenStumbleGuysWebsite() {
         try (Playwright playwright = Playwright.create()) {
+
             Browser browser = playwright.chromium()
                     .launch(new BrowserType.LaunchOptions().setHeadless(false));
 
             Page page = browser.newPage();
+
             page.navigate(Config.BASE_URL);
 
             HomePage homePage = new HomePage(page);
             homePage.acceptCookies();
+            homePage.openLogin();
 
             Assert.assertTrue(
                     page.url().contains("stumbleguys.com"),
